18208e7608930f1da9d1783b32befae2ecb45f04
Bit-Blocks <bit-blocks@outlook.com>
2024-05-06 18:01:19 -0500

    social buttons and external link hidden

--- src/config.ts
+++ src/config.ts
@@ -33,11 +33,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari', // Internal links should not include the base path, as it is automatically added
-      external: true, // Show an external link icon and will open in a new tab
-    },
+    // {
+    //   name: 'GitHub',
+    //   url: 'https://github.com', // Internal links should not include the base path, as it is automatically added
+    //   external: true, // Show an external link icon and will open in a new tab
+    // },
   ],
 }
 
@@ -47,22 +47,22 @@ export const profileConfig: ProfileConfig = {
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter', // Visit https://icones.js.org/ for icon codes
+      name: 'YouTube',
+      icon: 'fa6-brands:youtube', // Visit https://icones.js.org/ for icon codes
       // You will need to install the corresponding icon set if it's not already included
       // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://twitter.com',
     },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
-    },
+    // {
+    //   name: 'TikTok',
+    //   icon: 'fa6-brands:tiktok',
+    //   url: 'https://youtube.com',
+    // },
+    // {
+    //   name: 'GitHub',
+    //   icon: 'fa6-brands:github',
+    //   url: 'https://github.com',
+    // },
   ],
 }
 
