4419a61bb61e74b5ae38feba7003445543b21083
Bit-Blocks <bit-blocks@outlook.com>
2024-05-07 13:34:01 -0500

    The main information on the page is updated
    Changes:
    * Title and subtitle
    * Avatar image
    * Social Icons
    * Favicon route

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'es', // 'en', 'zh_CN', 'zh_TW', 'ja'
+  title: 'Buenas Prácticas Digitales',
+  subtitle: 'En el Hogar.',
+  lang: 'es', // 'es', 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 195, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true, // Hide the theme color picker for visitors
@@ -20,49 +20,32 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [
     // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+      src: '/favicon/icon.svg', // Path of the favicon, relative to the /public directory
+      sizes: '32x32', // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    },
   ],
 }
 
 export const navBarConfig: NavBarConfig = {
-  links: [
-    LinkPreset.Home,
-    LinkPreset.Archive,
-    LinkPreset.About,
-    // {
-    //   name: 'GitHub',
-    //   url: 'https://github.com', // Internal links should not include the base path, as it is automatically added
-    //   external: true, // Show an external link icon and will open in a new tab
-    // },
-  ],
+  links: [LinkPreset.Home, LinkPreset.Archive, LinkPreset.About],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/avatar.svg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Bit-Blocks',
+  bio: 'Sígueme en mis redes.',
   links: [
     {
       name: 'YouTube',
-      icon: 'fa6-brands:youtube', // Visit https://icones.js.org/ for icon codes
-      // You will need to install the corresponding icon set if it's not already included
-      // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      icon: 'fa6-brands:youtube',
+      url: '#',
     },
-    // {
-    //   name: 'TikTok',
-    //   icon: 'fa6-brands:tiktok',
-    //   url: 'https://youtube.com',
-    // },
-    // {
-    //   name: 'GitHub',
-    //   icon: 'fa6-brands:github',
-    //   url: 'https://github.com',
-    // },
+    {
+      name: 'Telegram',
+      icon: 'fa6-brands:telegram',
+      url: '#',
+    },
   ],
 }
 
